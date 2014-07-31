f18d95f91dfa764906bbbd7572adb95d52ada293
mhwheeler <mhwheeler@gmail.com>
2014-07-31 16:44:43 -0700

    code style

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -3,8 +3,8 @@ Errors = new Meteor.Collection(null);
 
 throwError = function(message) {
   Errors.insert({message: message, seen: false})
-}
+};
 
 clearErrors = function() {
   Errors.remove({seen: true});
-}
+};
